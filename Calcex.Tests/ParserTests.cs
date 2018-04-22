925bcea2ae22a0d5fa79c2375cc3373e208988a1
quanik <bluegrams@users.sourceforge.net>
2018-04-22 15:28:10 +0200

    Added project template.

---
+++ Calcex.Tests/ParserTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bluegrams.Calcex.Tests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
